a200031e8845e7da7d96cf8a801551b741b7146d
Matvey <sinema3d00@gmail.com>
2024-06-06 17:45:45 +0300

    made player score logic

---
+++ Assets/Scripts/Player/PlayerControllers/PlayerScore/PlayerScoreController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScoreController : MonoBehaviour
+{
+    public static PlayerScoreController Instance;
+    [SerializeField] private float _playerScore;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void AddScore(float value)
+    {
+        _playerScore += value;
+    }
+}
