b7601737151a47f76d089858a65bb4297e918385
Matvey <sinema3d00@gmail.com>
2024-06-09 10:17:21 +0300

    adjust mechanics

--- Assets/Scripts/DragableObjects/DragableObject.cs
+++ Assets/Scripts/DragableObjects/DragableObject.cs
@@ -1,42 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DragableObject : MonoBehaviour
 {
-    public bool IsPlayerOnObject { get; private set; }
-    private bool _isCollidingWithGround;
-
+    public bool IsPlayerOnObject;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            _isCollidingWithGround = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            _isCollidingWithGround = false;
-        }
-    }
-
-    public bool IsCollidingWithGround()
-    {
-        return _isCollidingWithGround;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             IsPlayerOnObject = true;
         }
     }
-
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             IsPlayerOnObject = false;
         }
