ce32e95a5050e0e2360dd87cd122ea74af2e3255
Matvey <sinema3d00@gmail.com>
2024-06-10 16:27:56 +0300

    made play button

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Button _playButton;
+
+    private void Awake()
     {
-        
+        _playButton.onClick.AddListener(() =>
+        {
+            Loader.Load(Loader.Scene.Level1);
+        });
+        Time.timeScale = 1.0f;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
