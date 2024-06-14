3aa4ab2ff9e69df67d39780c9eedfc2d438204e8
Matvey <sinema3d00@gmail.com>
2024-06-14 21:38:30 +0300

    adjust coin controller

--- Assets/Scripts/Coin/CoinController.cs
+++ Assets/Scripts/Coin/CoinController.cs
@@ -1,26 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
-    private PlayerScoreController _playerScoreController;
-    
-    [SerializeField] private LevelManager _levelManager;
+    private LevelManager _levelManager;
+    private PlayerScoreController _scoreController;
 
     [SerializeField] private float _coinValue;
     private void Start()
     {
-        _playerScoreController = PlayerScoreController.Instance;
+        _levelManager = FindObjectOfType<LevelManager>();
+        _scoreController = PlayerScoreController.Instance;
+        if (_levelManager == null)
+        {
+            Debug.LogError("LevelManager not found in the scene.");
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerMovementController>())
         {
-            _playerScoreController.AddScore(_coinValue);
-            _levelManager.CollectCoin();
-            Destroy(gameObject);
+            _scoreController.AddScore(_coinValue);
+            if (_levelManager != null)
+            {
+                _levelManager.CollectCoin();
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.LogWarning("LevelManager reference is missing in CoinController.");
+            }
         }
     }
 }
