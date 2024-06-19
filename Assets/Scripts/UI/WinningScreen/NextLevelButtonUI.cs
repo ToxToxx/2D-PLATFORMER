810539aa268072e4cf7d68519efbfa35828ef36b
Matvey <sinema3d00@gmail.com>
2024-06-19 20:51:50 +0300

    fixing bugs

--- Assets/Scripts/UI/WinningScreen/NextLevelButtonUI.cs
+++ Assets/Scripts/UI/WinningScreen/NextLevelButtonUI.cs
@@ -8,18 +8,43 @@ public class NextLevelButtonUI : MonoBehaviour
     [SerializeField] private Loader.Scene _nextLevel;
     [SerializeField] private LevelManager _levelManager;
     [SerializeField] private Button _nextLevelButton;
+
     private void LoadNextLevel()
     {
-        Loader.Load(_nextLevel);
-        _levelManager.HideWinningScreen();
-        Time.timeScale = 1.0f;
+        if (_levelManager != null)
+        {
+            Loader.Load(_nextLevel);
+            Time.timeScale = 1.0f;
+            _levelManager.HideWinningScreen();
+        }
+        else
+        {
+            Debug.LogError("NextLevel or LevelManager reference is missing.");
+        }
     }
 
     private void OnEnable()
     {
-        _nextLevelButton.onClick.AddListener(() =>
+        if (_nextLevelButton != null)
         {
-            LoadNextLevel();
-        });
+            _nextLevelButton.onClick.AddListener(OnNextLevelButtonClick);
+        }
+        else
+        {
+            Debug.LogError("NextLevelButton reference is missing.");
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_nextLevelButton != null)
+        {
+            _nextLevelButton.onClick.RemoveListener(OnNextLevelButtonClick);
+        }
+    }
+
+    private void OnNextLevelButtonClick()
+    {
+        LoadNextLevel();
     }
 }
